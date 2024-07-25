b1df7ae4d34c460a8b416918d292bb35595aead5
John Doe <nickborcic69@gmail.com>
2024-07-26 00:03:02 +0200

    new wordList added to NewWords.json,

--- src/components/NewWords.jsx
+++ src/components/NewWords.jsx
@@ -1,37 +1,41 @@
 import React from "react";
-
+import wordList from "/public/NewWords.json";
+//make a list of new words in a 3word object and display them in a list of new words
+//after 5min of wait time, the list will be updated with new words
 const NewWords = () => {
   return (
-    <div className="flex justify-center items-center rounded bg-blue-300 p-2  border flex-col border-black">
-      <section className="mb-2">
-        <p className="font-bold">New word One</p>
-        <p>Definition</p>
-      </section>
-      <section>
-        <li>Variation One</li>
-        <li>Variation Two</li>
-        <li>Variation Three</li>
-      </section>
-      <br />
-      <section className="mb-2">
-        <p className="font-bold">New word One</p>
-        <p>Definition</p>
-      </section>
-      <section>
-        <li>Variation One</li>
-        <li>Variation Two</li>
-        <li>Variation Three</li>
-      </section>
-      <br />
-      <section className="mb-2">
-        <p className="font-bold">New word One</p>
-        <p>Definition</p>
-      </section>
-      <section>
-        <li>Variation One</li>
-        <li>Variation Two</li>
-        <li>Variation Three</li>
-      </section>
+    <div>
+      <div className="flex justify-center items-center rounded bg-blue-300 p-2  border flex-col border-black">
+        <section className="mb-2">
+          <p className="font-bold">New word One</p>
+          <p>Definition</p>
+        </section>
+        <section>
+          <li>Variation One</li>
+          <li>Variation Two</li>
+          <li>Variation Three</li>
+        </section>
+        <br />
+        <section className="mb-2">
+          <p className="font-bold">New word One</p>
+          <p>Definition</p>
+        </section>
+        <section>
+          <li>Variation One</li>
+          <li>Variation Two</li>
+          <li>Variation Three</li>
+        </section>
+        <br />
+        <section className="mb-2">
+          <p className="font-bold">New word One</p>
+          <p>Definition</p>
+        </section>
+        <section>
+          <li>Variation One</li>
+          <li>Variation Two</li>
+          <li>Variation Three</li>
+        </section>
+      </div>
     </div>
   );
 };
