6b35dc51f145ee8fc747d4b618021118f626f8c0
John Doe <nickborcic69@gmail.com>
2024-07-27 23:21:41 +0200

    new worsd variations missing, some style changes

--- src/components/NewWords.jsx
+++ src/components/NewWords.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import wordList from "/public/NewWords.json";
-//make a list of new words in a 3word object and display them in a list of new words
-//after 5min of wait time, the list will be updated with new words
+
 const NewWords = () => {
+  const firstRandom = Math.floor(Math.random() * wordList.length);
+  const secondRandom = Math.floor(Math.random() * wordList.length);
+  const thirdRandom = Math.floor(Math.random() * wordList.length);
+
+  const word = wordList[firstRandom].word;
+  const wordd = wordList[secondRandom].word;
+  const worddd = wordList[thirdRandom].word;
+
+  const def = wordList[firstRandom].definition;
+  const deff = wordList[secondRandom].definition;
+  const defff = wordList[thirdRandom].definition;
   return (
     <div>
       <div className="flex justify-center items-center rounded bg-blue-300 p-2  border flex-col border-black">
         <section className="mb-2">
-          <p className="font-bold">New word One</p>
-          <p>Definition</p>
+          <p className="font-bold flex justify-center items-center">{word}</p>
+          <p className="flex justify-center items-center w-[80vw]">
+            {def}
+          </p>
         </section>
         <section>
           <li>Variation One</li>
@@ -17,8 +29,8 @@ const NewWords = () => {
         </section>
         <br />
         <section className="mb-2">
-          <p className="font-bold">New word One</p>
-          <p>Definition</p>
+          <p className="font-bold flex justify-center items-center">{wordd}</p>
+          <p className="flex justify-center items-center w-[80vw]">{deff}</p>
         </section>
         <section>
           <li>Variation One</li>
@@ -27,8 +39,8 @@ const NewWords = () => {
         </section>
         <br />
         <section className="mb-2">
-          <p className="font-bold">New word One</p>
-          <p>Definition</p>
+          <p className="font-bold flex justify-center items-center">{worddd}</p>
+          <p className="flex justify-center items-center w-[80vw]">{defff}</p>
         </section>
         <section>
           <li>Variation One</li>
