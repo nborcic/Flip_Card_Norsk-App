836c5e7ce9ed69fbb7a71b3d25915bca5b05b167
John Doe <nickborcic69@gmail.com>
2024-07-29 00:02:12 +0200

    new words set, css next

--- src/components/NewWords.jsx
+++ src/components/NewWords.jsx
@@ -7,46 +7,57 @@ const NewWords = () => {
   const thirdRandom = Math.floor(Math.random() * wordList.length);
 
   const word = wordList[firstRandom].word;
-  const wordd = wordList[secondRandom].word;
-  const worddd = wordList[thirdRandom].word;
-
   const def = wordList[firstRandom].definition;
+  const varr0 = wordList[firstRandom].variations[0];
+  const varr1 = wordList[firstRandom].variations[1];
+  const varr2 = wordList[firstRandom].variations[2];
+
+  const wordd = wordList[secondRandom].word;
   const deff = wordList[secondRandom].definition;
+  const varrr0 = wordList[secondRandom].variations[0];
+  const varrr1 = wordList[secondRandom].variations[1];
+  const varrr2 = wordList[secondRandom].variations[2];
+
+  const worddd = wordList[thirdRandom].word;
   const defff = wordList[thirdRandom].definition;
+  const varrrr0 = wordList[thirdRandom].variations[0];
+  const varrrr1 = wordList[thirdRandom].variations[1];
+  const varrrr2 = wordList[thirdRandom].variations[2];
+
   return (
     <div>
       <div className="flex justify-center items-center rounded bg-blue-300 p-2  border flex-col border-black">
+      <br />
         <section className="mb-2">
-          <p className="font-bold flex justify-center items-center">{word}</p>
-          <p className="flex justify-center items-center w-[80vw]">
-            {def}
-          </p>
+          <p className="font-bold text-xl flex justify-center items-center">{word}</p>
+          <p className="flex justify-center items-center w-[80vw]">{def}</p>
         </section>
         <section>
-          <li>Variation One</li>
-          <li>Variation Two</li>
-          <li>Variation Three</li>
+          <li>{varr0}</li>
+          <li>{varr1}</li>
+          <li>{varr2}</li>
         </section>
         <br />
         <section className="mb-2">
-          <p className="font-bold flex justify-center items-center">{wordd}</p>
+          <p className="font-bold text-xl flex justify-center items-center">{wordd}</p>
           <p className="flex justify-center items-center w-[80vw]">{deff}</p>
         </section>
         <section>
-          <li>Variation One</li>
-          <li>Variation Two</li>
-          <li>Variation Three</li>
+          <li>{varrr0}</li>
+          <li>{varrr1}</li>
+          <li>{varrr2}</li>
         </section>
         <br />
         <section className="mb-2">
-          <p className="font-bold flex justify-center items-center">{worddd}</p>
+          <p className="font-bold text-xl flex justify-center items-center">{worddd}</p>
           <p className="flex justify-center items-center w-[80vw]">{defff}</p>
         </section>
         <section>
-          <li>Variation One</li>
-          <li>Variation Two</li>
-          <li>Variation Three</li>
+          <li>{varrrr0}</li>
+          <li>{varrrr1}</li>
+          <li>{varrrr2}</li>
         </section>
+        <br />
       </div>
     </div>
   );
